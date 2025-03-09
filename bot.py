908c44e990f22ced16368e970da04c4abbaa35e1
elbek2327 <zubaydullayev1609@gmail.com>
2025-03-09 14:49:59 +0500

    wikipedia bot worked

---
+++ bot.py
@@ -0,0 +1,62 @@
+import asyncio
+import logging
+import sys
+import wikipedia
+
+
+# from os import getenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+
+wikipedia.set_lang('en')
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = "7825633711:AAFO6c5oOnjuco2BzSwkWYyKDY1jFWiI-kU"
+
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def send_message(message: Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer("Can not foung anything on this topic:(")
+
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
---
+++ wikitest.py
@@ -0,0 +1,6 @@
+import wikipedia
+
+wikipedia.set_lang('en')
+
+print(wikipedia.search('Tashkent'))
+print(wikipedia.summary('Tashkent'))
